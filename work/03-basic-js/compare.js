b3c5b2cb354e16ec4439abc5505bdf6d81972ded
gayatridt <gayatri.dubey@gmail.com>
2025-01-29 16:38:27 -0800

    Completes the basic-js assignment

--- work/03-basic-js/compare.js
+++ work/03-basic-js/compare.js
@@ -6,5 +6,52 @@ function compare( word, guess ) {  // DO NOT MODIFY THIS LINE
 
 /* YOU MAY MODIFY THE LINES BELOW */
 
-  return 0; // this line is wrong
+  const firstWord = word.toUpperCase();
+  const secondWord = guess.toUpperCase();
+  
+  const firstWordLetters = {};
+  const secondWordLetters = {};
+  
+  for (const letter of firstWord) 
+  {
+    if (!firstWordLetters[letter]) 
+    {
+      firstWordLetters[letter] = 1;
+    } 
+    else 
+    {
+      firstWordLetters[letter] += 1;
+    }
+  }
+  
+  for (const letter of secondWord) 
+  {
+    if (!secondWordLetters[letter]) 
+    {
+      secondWordLetters[letter] = 1;
+    } 
+    else 
+    {
+      secondWordLetters[letter] += 1;
+    }
+  }
+  
+  let matches = 0;
+  
+  for (const letter of Object.keys(firstWordLetters)) 
+  {
+    if (secondWordLetters[letter]) 
+    {
+      if (firstWordLetters[letter] < secondWordLetters[letter])
+      {
+        matches += firstWordLetters[letter];
+      } 
+      else 
+      {
+        matches += secondWordLetters[letter];
+      }
+    }
+  }
+  
+  return matches;
 }
